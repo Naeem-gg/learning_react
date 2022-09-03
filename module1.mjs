e75c79f9030566b4f1642eeb971e9e9e8286e9f4
Naeemgg <naeemgg@duck.com>
2022-09-03 16:13:27 +0530

    learning props and module import exports

---
+++ module1.mjs
@@ -0,0 +1,2 @@
+import sn from './module2.mjs';
+console.log(sn);
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+const name="Naeem";
+const first = "firstName";
+const second = "secondName";
+console.log(first)
+export default second;
